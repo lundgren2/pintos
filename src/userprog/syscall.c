400bd87e165a2e5bb0efeb8e2569a0572f238c3c
Oscar Klein <osckl172@student.liu.se>
2018-02-08 13:36:02 +0100

    Fix wrong include in syscall.c

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -16,17 +16,15 @@
 
 //NEW
 #include "lib/syscall-nr.h"
-#include "threads/init.h"
-
 #include "userprog/flist.h"
 #include "filesys/directory.h"
 #include "filesys/inode.h"
 
-//#include "devices/timer.c"
+#include "devices/timer.h"
 
 static void syscall_handler (struct intr_frame *);
 
-  void
+void
 syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -34,14 +32,14 @@ syscall_init (void)
 
 
 /* This array defined the number of arguments each syscall expects.
-   For example, if you want to find out the number of arguments for
-   the read system call you shall write:
+For example, if you want to find out the number of arguments for
+the read system call you shall write:
 
-   int sys_read_arg_count = argc[ SYS_READ ];
+int sys_read_arg_count = argc[ SYS_READ ];
 
-   All system calls have a name such as SYS_READ defined as an enum
-   type, see `lib/syscall-nr.h'. Use them instead of numbers.
- */
+All system calls have a name such as SYS_READ defined as an enum
+type, see `lib/syscall-nr.h'. Use them instead of numbers.
+*/
 const int argc[] = {
   /* basic calls */
   0, 1, 1, 1, 2, 1, 1, 1, 3, 3, 2, 1, 1,
@@ -54,12 +52,12 @@ static int32_t sys_read_(int32_t FD, char * buf , const unsigned size){
   int32_t i = 0;
   struct thread* tr = thread_current();
   struct map* m = &tr->file_map;
-  if(map_find(m, (int)FD) != -1){ //Filen Ã¶ppnad
+  if(map_find(m, (int)FD) != -1){ // Filen Öppnad
     struct file* f = map_find(m, FD);
     if(f != NULL) return (int32_t)file_read(f, buf, size);
     else return -1;
 
-  }else{ //Filen inte Ã¶ppnad
+  }else{ // Filen inte Öppnad
     return -1;
   }
 }
@@ -68,12 +66,12 @@ static int32_t sys_write_(int32_t FD, char * buf , const unsigned size){
   int32_t i = 0;
   struct thread* tr = thread_current();
   struct map* m = &tr->file_map;
-  if(map_find(m, (int)FD) != -1){ //Filen öppnad FD != STDIN_FILENO
+  if(map_find(m, (int)FD) != -1){ // Filen öppnad FD != STDIN_FILENO
     struct file* f = map_find(m, FD);
     if (f != NULL) return (int32_t)file_write(f, buf, size);
     else return -1;
 
-  }else{ //Filen inte öppnad
+  }else{ // Filen inte öppnad
     return -1;
   }
 
@@ -85,18 +83,18 @@ static int32_t sys_keyboard_read_(char * FD, char * buf , const unsigned size){
   unsigned i = 0;
   char c;
   while(i<size){
-          c = (char)input_getc();
-          if(c == '\r'){
-            c = '\n';
-          }
-          *buf = c;
-  //  int32_t intpath = (int32_t)esp[1];
-          putbuf(buf, (size_t)1);
-          ++buf;
-          ++i;
-          //if(c == '\n')
-            //break;
-        }
+    c = (char)input_getc();
+    if(c == '\r'){
+      c = '\n';
+    }
+    *buf = c;
+    //  int32_t intpath = (int32_t)esp[1];
+    putbuf(buf, (size_t)1);
+    ++buf;
+    ++i;
+    //if(c == '\n')
+    //break;
+  }
   *buf = '\0';
   return (int32_t)i;
 }
@@ -128,7 +126,7 @@ void sys_seek_(int FD, unsigned pos){
 }
 
 unsigned sys_tell_(int FD){
-   struct thread* tr = thread_current();
+  struct thread* tr = thread_current();
   struct map* m = &tr->file_map;
   struct file* fil = map_find(m, FD);
   int i = 0;
@@ -140,7 +138,7 @@ unsigned sys_tell_(int FD){
 }
 
 unsigned sys_filesize_(int FD){
-   struct thread* tr = thread_current();
+  struct thread* tr = thread_current();
   struct map* m = &tr->file_map;
   struct file* fil = map_find(m, FD);
   if(fil != -1){
@@ -150,6 +148,11 @@ unsigned sys_filesize_(int FD){
   }
 }
 
+// NEW 31 aug 2017
+void sys_plist_ (void) {
+  // Snygg utskrift
+  process_print_list();
+}
 
 static void
 syscall_handler (struct intr_frame *f)
@@ -158,16 +161,17 @@ syscall_handler (struct intr_frame *f)
 
   switch ( *esp /* retrive syscall number */ )
   {
-  case SYS_HALT :
+    case SYS_HALT :
     power_off();
     break;
-  case SYS_EXIT:
+    case SYS_EXIT:
+    // TODO: process_cleanup: http://www.ida.liu.se/~TDIU16/2017/lab/pdf/17sysexec.pdf
     printf("thread_exit()... %i\n", esp[1]);
     thread_exit();
     printf("thread_exit() done...\n");
     break;
 
-  case SYS_READ :
+    case SYS_READ :
     //läs fil
     NULL;
     {
@@ -193,14 +197,13 @@ syscall_handler (struct intr_frame *f)
       }
       break;
     }
-  case SYS_WRITE :
+    case SYS_WRITE :
     //skriv ti
     NULL;
     {
       int32_t FD = (int32_t)esp[1];
       if(FD != STDIN_FILENO){
         if(FD == STDOUT_FILENO){
-          //printf("STDOUT");
           int32_t buffer = (int32_t)esp[2];
           int32_t len = (int32_t)esp[3];
           int32_t nr_bytes = sys_console_write_((char *)FD, (char *)buffer, (unsigned)len);
@@ -219,83 +222,101 @@ syscall_handler (struct intr_frame *f)
       }
       break;
     }
-  case SYS_OPEN : //Open a file
-  NULL;
-  {
-    int32_t intpath = (int32_t)esp[1];
-    int32_t retval = (int32_t)sys_open_file_((char*)intpath);
-    f->eax = retval;
-    break;
-  }
-  case SYS_CLOSE :
-  NULL;
-  {
-    int32_t fd = (int32_t)esp[1];
-    struct thread *t = thread_current();
-    struct map *m = &t->file_map;
-    struct file * f = map_remove(m,fd);
+    case SYS_OPEN : //Open a file
+    NULL;
+    {
+      int32_t intpath = (int32_t)esp[1];
+      int32_t retval = (int32_t)sys_open_file_((char*)intpath);
+      f->eax = retval;
+      break;
+    }
+    case SYS_CLOSE :
+    NULL;
+    {
+      int32_t fd = (int32_t)esp[1];
+      struct thread *t = thread_current();
+      struct map *m = &t->file_map;
+      struct file * f = map_remove(m,fd);
 
-    break;
-  }
-  case SYS_REMOVE :
-  NULL;
-  {
-    int32_t intpath = (int32_t)esp[1];
-    f->eax = (int32_t)filesys_remove((char*)intpath);
-    break;
-  }
-  case SYS_CREATE :
-  NULL;
-  {
-    int32_t intpath = (int32_t)esp[1];
-    int32_t size = (int32_t)esp[2];
-    f->eax = (int32_t)filesys_create ((char*)intpath, (off_t)size);
-    break;
-  }
-  case SYS_SEEK :
-  NULL;
-  {
-    int32_t FD = esp[1];
-    int32_t pos = esp[2];
-    sys_seek_((int)FD, (unsigned) pos);
-    break;
-  }
-  case SYS_TELL :
-  NULL;
-  {
-        int32_t FD = esp[1];
-        f->eax = sys_tell_((int)FD);
-    break;
-  }
-  case SYS_FILESIZE :
-  NULL;
-  {
-    int32_t FD = esp[1];
-    f->eax = sys_filesize_((int)FD);
+      break;
+    }
+    case SYS_REMOVE :
+    NULL;
+    {
+      int32_t intpath = (int32_t)esp[1];
+      f->eax = (int32_t)filesys_remove((char*)intpath);
+      break;
+    }
+    case SYS_CREATE :
+    NULL;
+    {
+      int32_t intpath = (int32_t)esp[1];
+      int32_t size = (int32_t)esp[2];
+      f->eax = (int32_t)filesys_create ((char*)intpath, (off_t)size);
+      break;
+    }
+    case SYS_SEEK :
+    NULL;
+    {
+      int32_t FD = esp[1];
+      int32_t pos = esp[2];
+      sys_seek_((int)FD, (unsigned) pos);
+      break;
+    }
+    case SYS_TELL :
+    NULL;
+    {
+      int32_t FD = esp[1];
+      f->eax = sys_tell_((int)FD);
+      break;
+    }
+    case SYS_FILESIZE :
+    NULL;
+    {
+      int32_t FD = esp[1];
+      f->eax = sys_filesize_((int)FD);
 
-    break;
-  }
-  case SYS_EXEC :
-  NULL;
-  {
-    int32_t cml = esp[1];
-    int32_t id = process_execute((const char *)cml);
-    break;
-  }
-  case SYS_SLEEP :
-  NULL;
-  {
-    int32_t millis = esp[1];
-    timer_msleep((int64_t)millis);
-    break;
-  }
-  case SYS_PLIST :
-  NULL;
-  {
+      break;
+    }
+    case SYS_EXEC :
+    NULL;
+    {
 
-    break;
-  }
-default:
+      /* Men nu skall den nya processen läggas till i en
+      lista över aktiva processer. Ett lämpligt process-id är nu processens index i listan, men
+      det går att lösa på många sätt (valfritt). För att kunna hålla reda på relationen mellan
+      processer behöver varje process hålla reda på sitt namn, sitt eget process-id, sin
+      förälders process-id, samt eventuell extra data som en process behöver skicka tillbaka
+      till sin förälderprocess eller tvärtom (jämför med vad du gjorde i uppgift 10). */
+
+      char* cml = (char*)esp[1]; // ESP index?
+
+      // Check if esp[1] is valid pointer.
+      if (cml == NULL) {
+        printf("SYS_EXEC: ESP1 fails");
+        f->eax = -1;
+        thread_exit();
+        break;
+      }
+      uint32_t id = process_execute(cml);
+      f->eax = id;
+      break;
+    }
+    case SYS_SLEEP :
+    NULL;
+    {
+      int32_t time = esp[1];
+      timer_msleep((int64_t)time);
+      break;
+    }
+    case SYS_PLIST :
+    NULL;
+    {
+      sys_plist_();
+
+      break;
+    }
+    default:
     {
       printf ("Executed an unknown system call!\n");
 
