18f7a5980a616dd379ac40323bb954fce0220005
Tobias Lundgren <toblu933@student.liu.se>
2018-04-24 17:16:37 +0200

    Refactor of syscall_handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -47,26 +47,22 @@ const int argc[] = {
     0};
 static int32_t sys_read_(int32_t FD, char *buf, const unsigned size)
 {
-  int32_t i = 0;
   struct thread *tr = thread_current();
   struct map *m = &tr->file_map;
   if (map_find(m, (int)FD) != -1)
   { // Filen Öppnad
     struct file *f = map_find(m, FD);
     if (f != NULL)
+    {
       return (int32_t)file_read(f, buf, size);
-    else
-      return -1;
+    }
   }
-  else
-  { // Filen inte Öppnad
-    return -1;
-  }
+  // Filen inte Öppnad
+  return -1;
 }
 
 static int32_t sys_write_(int32_t FD, char *buf, const unsigned size)
 {
-  int32_t i = 0;
   struct thread *tr = thread_current();
   struct map *m = &tr->file_map;
   if (map_find(m, (int)FD) != -1)
@@ -74,14 +70,9 @@ static int32_t sys_write_(int32_t FD, char *buf, const unsigned size)
     struct file *f = map_find(m, FD);
     if (f != NULL)
       return (int32_t)file_write(f, buf, size);
-    else
-      return -1;
   }
-  else
-  { // Filen inte öppnad
-    return -1;
-  }
-  return (int32_t)i;
+  // Filen inte öppnad
+  return -1;
 }
 
 static int32_t sys_keyboard_read_(char *FD, char *buf, const unsigned size)
@@ -96,12 +87,9 @@ static int32_t sys_keyboard_read_(char *FD, char *buf, const unsigned size)
       c = '\n';
     }
     *buf = c;
-    //  int32_t intpath = (int32_t)esp[1];
     putbuf(buf, (size_t)1);
     ++buf;
     ++i;
-    //if(c == '\n')
-    //break;
   }
   *buf = '\0';
   return (int32_t)i;
@@ -114,7 +102,8 @@ static int32_t sys_open_file_(char *filepath)
   if (f != NULL)
   {
     int32_t retval = map_insert(m, f);
-    return retval; // -1 if map_insert fails
+    // -1 if map_insert fails
+    return retval;
   }
   else
   {
@@ -169,20 +158,22 @@ unsigned sys_filesize_(int FD)
   }
 }
 
-// NEW 31 aug 2017
 void sys_plist_(void)
 {
   process_print_list();
 }
 
-// TODO: Refactor syscall handler to be more readable!!
-static void
-syscall_handler(struct intr_frame *f)
+static void syscall_handler(struct intr_frame *f)
 {
   int32_t *esp = (int32_t *)f->esp;
-
+  int32_t FD = (int32_t)esp[1];
+  int32_t buffer = (int32_t)esp[2];
+  int32_t len = (int32_t)esp[3];
+  char *cml = (char *)esp[1];
+  struct thread *t = thread_current();
   switch (*esp /* retrive syscall number */)
   {
+
   case SYS_HALT:
     power_off();
     break;
@@ -194,168 +185,95 @@ syscall_handler(struct intr_frame *f)
     break;
 
   case SYS_READ:
-    //läs fil
+    if (FD != STDOUT_FILENO)
     {
-      int32_t FD = (int32_t)esp[1];
-      if (FD != STDOUT_FILENO)
+      if (FD == STDIN_FILENO)
       {
-        if (FD == STDIN_FILENO)
-        {
-          //printf("STDIN");
-          int32_t buffer = (int32_t)esp[2];
-          int32_t len = (int32_t)esp[3];
-          int32_t nr_bytes = sys_keyboard_read_((char *)FD, (char *)buffer, (unsigned)len);
-          f->eax = nr_bytes;
-        }
-        else
-        { // om fil
-          int32_t buffer = (int32_t)esp[2];
-          int32_t len = (int32_t)esp[3];
-          int32_t nr_bytes = sys_read_(FD, (char *)buffer, (unsigned)len);
-          f->eax = nr_bytes;
-        }
+        int32_t nr_bytes = sys_keyboard_read_((char *)FD, (char *)buffer, (unsigned)len);
+        f->eax = nr_bytes;
       }
       else
-      {
-        //return -1
-        f->eax = -1;
-        printf("NEEEEJ READ\n");
+      { // om fil
+        int32_t nr_bytes = sys_read_(FD, (char *)buffer, (unsigned)len);
+        f->eax = nr_bytes;
       }
-      break;
     }
+    else
+    {
+      f->eax = -1;
+    }
+    break;
   case SYS_WRITE:
-    //skriv ti
-    NULL;
+    if (FD != STDIN_FILENO)
     {
-      int32_t FD = (int32_t)esp[1];
-      if (FD != STDIN_FILENO)
+      if (FD == STDOUT_FILENO)
       {
-        if (FD == STDOUT_FILENO)
-        {
-          int32_t buffer = (int32_t)esp[2];
-          int32_t len = (int32_t)esp[3];
-          int32_t nr_bytes = sys_console_write_((char *)FD, (char *)buffer, (unsigned)len);
-          f->eax = nr_bytes;
-        }
-        else
-        { //Hantera fil istället
-          int32_t buffer = (int32_t)esp[2];
-          int32_t len = (int32_t)esp[3];
-          int32_t nr_bytes = sys_write_(FD, (char *)buffer, (unsigned)len);
-          f->eax = nr_bytes;
-        }
+        int32_t nr_bytes = sys_console_write_((char *)FD, (char *)buffer, (unsigned)len);
+        f->eax = nr_bytes;
       }
       else
-      {
-        //return -1
-        f->eax = -1;
-        printf("NEEEEJ WRITE\n");
+      { //Hantera fil istället
+        int32_t nr_bytes = sys_write_(FD, (char *)buffer, (unsigned)len);
+        f->eax = nr_bytes;
       }
-      break;
     }
+    else
+    {
+      f->eax = -1;
+    }
+    break;
   case SYS_OPEN: //Open a file
-    NULL;
-    {
-      int32_t intpath = (int32_t)esp[1];
-      int32_t retval = (int32_t)sys_open_file_((char *)intpath);
-      f->eax = retval;
-      break;
-    }
+    f->eax = (int32_t)sys_open_file_((char *)FD);
+    break;
   case SYS_CLOSE:
-    NULL;
-    {
-      int32_t fd = (int32_t)esp[1];
-      struct thread *t = thread_current();
-      struct map *m = &t->file_map;
-      struct file *f = map_remove(m, fd);
-
-      break;
-    }
+    map_remove(&t->file_map, FD);
+    break;
   case SYS_REMOVE:
-    NULL;
-    {
-      int32_t intpath = (int32_t)esp[1];
-      f->eax = (int32_t)filesys_remove((char *)intpath);
-      break;
-    }
+    f->eax = (int32_t)filesys_remove((char *)FD);
+    break;
   case SYS_CREATE:
-    NULL;
-    {
-      int32_t intpath = (int32_t)esp[1];
-      int32_t size = (int32_t)esp[2];
-      f->eax = (int32_t)filesys_create((char *)intpath, (off_t)size);
-      break;
-    }
+    f->eax = (int32_t)filesys_create((char *)FD, (off_t)buffer);
+    break;
   case SYS_SEEK:
-    NULL;
-    {
-      int32_t FD = esp[1];
-      int32_t pos = esp[2];
-      sys_seek_((int)FD, (unsigned)pos);
-      break;
-    }
+    sys_seek_((int)FD, (unsigned)buffer);
+    break;
   case SYS_TELL:
-    NULL;
-    {
-      int32_t FD = esp[1];
-      f->eax = sys_tell_((int)FD);
-      break;
-    }
+    f->eax = sys_tell_((int)FD);
+    break;
   case SYS_FILESIZE:
-    NULL;
-    {
-      int32_t FD = esp[1];
-      f->eax = sys_filesize_((int)FD);
-
-      break;
-    }
+    f->eax = sys_filesize_((int)FD);
+    break;
   case SYS_EXEC:
-    NULL;
-    {
-
-      /* Men nu skall den nya processen läggas till i en
+    /* Men nu skall den nya processen läggas till i en
       lista över aktiva processer. Ett lämpligt process-id är nu processens index i listan, men
       det går att lösa på många sätt (valfritt). För att kunna hålla reda på relationen mellan
       processer behöver varje process hålla reda på sitt namn, sitt eget process-id, sin
       förälders process-id, samt eventuell extra data som en process behöver skicka tillbaka
       till sin förälderprocess eller tvärtom (jämför med vad du gjorde i uppgift 10). */
 
-      char *cml = (char *)esp[1]; // ESP index?
-
-      // Check if esp[1] is valid pointer.
-      if (cml == NULL)
-      {
-        printf("SYS_EXEC: ESP1 fails");
-        f->eax = -1;
-        thread_exit();
-        break;
-      }
-      uint32_t id = process_execute(cml);
-      f->eax = id;
+    // Check if esp[1] is valid pointer.
+    if (cml == NULL)
+    {
+      printf("SYS_EXEC: ESP1 fails");
+      f->eax = -1;
+      thread_exit();
       break;
     }
+    uint32_t id = process_execute(cml);
+    f->eax = id;
+    break;
   case SYS_SLEEP:
-    NULL;
-    {
-      int32_t time = esp[1];
-      timer_msleep((int64_t)time);
-      break;
-    }
+    timer_msleep((int64_t)FD);
+    break;
   case SYS_PLIST:
-    NULL;
-    {
-      sys_plist_();
-
-      break;
-    }
+    sys_plist_();
+    break;
   default:
-  {
     printf("Executed an unknown system call!\n");
 
     printf("Stack top + 0: %d\n", esp[0]);
     printf("Stack top + 1: %d\n", esp[1]);
 
     thread_exit();
-  }
   }
 }
