96761e0ca8207293b5f400be37c041b56dfcc1cd
Tobias Lundgren <toblu933@student.liu.se>
2018-05-23 19:50:30 +0200

    Lab5

--- src/standalone/upg5/verify_adr.c
+++ src/standalone/upg5/verify_adr.c
@@ -18,55 +18,97 @@
  *
  *  gcc -Wall -Wextra -std=gnu99 -pedantic -m32 -g pagedir.o verify_adr.c
  */
-#error Read comment above and then remove this line.
+// #error Read comment above and then remove this line.
 
 /* Verify all addresses from and including 'start' up to but excluding
  * (start+length). */
-bool verify_fix_length(void* start, int length)
+bool verify_fix_length(void *start, int length)
 {
-  // ADD YOUR CODE HERE
+  char *addr = (char *)pg_round_down(start);
+  char *end_addr = (char *)(start + length);
+  while (addr < end_addr)
+  {
+    bool check = pagedir_get_page(thread_current()->pagedir, (void *)addr) == NULL;
+    if (check)
+    {
+      return false;
+    }
+    else
+    {
+      addr = addr + PGSIZE;
+    }
+  }
+  return true; // If no problems return true
 }
 
 /* Verify all addresses from and including 'start' up to and including
  * the address first containg a null-character ('\0'). (The way
  * C-strings are stored.)
  */
-bool verify_variable_length(char* start)
+// Check: test
+bool verify_variable_length(char *start)
 {
-  // ADD YOUR CODE HERE
+  bool check = pagedir_get_page(thread_current()->pagedir, (void *)start) == NULL;
+  if (check)
+  {
+    return false;
+  }
+  else
+  {
+    char *addr = start;
+    int pagenum = pg_no(start);
+    int prevpage;
+    while (true)
+    {
+      prevpage = pg_no(addr);
+      if (pagenum != prevpage)
+      {
+        bool addr_check = pagedir_get_page(thread_current()->pagedir, (void *)addr) == NULL;
+        if (addr_check)
+        {
+          return false;
+        }
+        prevpage = pg_no(pagenum);
+      }
+      if (is_end_of_string(addr))
+      {
+        return true;
+      }
+      addr++;
+    }
+  }
 }
 
 /* Definition of test cases. */
 struct test_case_t
 {
-  void* start;
+  void *start;
   unsigned length;
 };
 
 #define TEST_CASE_COUNT 6
 
 const struct test_case_t test_case[TEST_CASE_COUNT] =
-{
-  {(void*)100, 100}, /* one full page */
-  {(void*)199, 102},
-  {(void*)101, 98},
-  {(void*)250, 190},
-  {(void*)250, 200},
-  {(void*)250, 210}
-};
+    {
+        {(void *)100, 100}, /* one full page */
+        {(void *)199, 102},
+        {(void *)101, 98},
+        {(void *)250, 190},
+        {(void *)250, 200},
+        {(void *)250, 210}};
 
 /* This main program will evalutate your solution. */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   int i;
   bool result;
 
-  if ( argc == 2 )
+  if (argc == 2)
   {
-    simulator_set_pagefault_time( atoi(argv[1]) );
+    simulator_set_pagefault_time(atoi(argv[1]));
   }
   thread_init();
-  
+
   /* Test the algorithm with a given intervall (a buffer). */
   for (i = 0; i < TEST_CASE_COUNT; ++i)
   {
@@ -75,7 +117,7 @@ int main(int argc, char* argv[])
     evaluate(result);
     end_evaluate_algorithm();
   }
-    
+
   /* Test the algorithm with a C-string (start address with
    * terminating null-character).
    */
@@ -83,7 +125,7 @@ int main(int argc, char* argv[])
   {
     start_evaluate_algorithm(test_case[i].start, test_case[i].length);
     result = verify_variable_length(test_case[i].start);
-    evaluate(result);    
+    evaluate(result);
     end_evaluate_algorithm();
   }
   return 0;
